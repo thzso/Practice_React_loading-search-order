fad8cdb252af445bc034dcd51c68a60faa5f9370
unknown <thuranszkyzso@gmail.com>
2023-03-22 16:18:52 +0100

    Updates

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-import "./style.css";
-
-import { makeServer } from './api/mockserver'
-makeServer({ environment: 'development' })
-
-ReactDOM.render(<App />, document.getElementById("root"));
